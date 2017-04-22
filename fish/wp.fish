cbc7a7b82e5d436bc5d0770312a63df982bf949a
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2017-04-23 00:09:39 +0300

    Complete subcommands.

--- fish/wp.fish
+++ fish/wp.fish
@@ -4,7 +4,7 @@
 ### Put this file to $HOME/.config/fish/competions directory.
 ###
 
-function __fish_wp_cli_needs_command
+function __fish_wp_cli_process_command
     set cmd (commandline -opc)
     if [ (count $cmd) -eq 1 -a $cmd[1] = 'wp' ]
         return 0
@@ -15,10 +15,29 @@ function __fish_wp_cli_needs_command
     return 1
 end
 
-set wp (wp | awk 'BEGIN {g=0} /^SUBCOMMANDS/ {g=1;next} /^[A-Z]+/ {g=0} /^$/ {next} {if (g) print $0}')
+function __fish_wp_cli_get_complete
+    if [ (count $argv) ]
+        set subcmd help $argv[1]
+    end
+    wp $subcmd | awk 'BEGIN {g=0} /^SUBCOMMANDS/ {g=1;next} /^[A-Z]+/ {g=0} /^$/ {next} {if (g) print $0}'
+end
 
-for i in $wp
+function __fish_wp_cli_using_command
+  set cmd (commandline -opc)
+  if [ (count $cmd) -gt 1 ]
+    if [ $argv[1] = $cmd[2] ]
+      return 0
+    end
+  end
+  return 1
+end
+
+for i in (__fish_wp_cli_get_complete)
     echo $i | read cmd desc
-    complete -f -c wp -n __fish_wp_cli_needs_command -a $cmd -d "$desc"
+    complete -f -c wp -n __fish_wp_cli_process_command -a $cmd -d "$desc"
+    for x in (__fish_wp_cli_get_complete $cmd)
+        echo $x | read subcmd desc
+        complete -f -c wp -n "__fish_wp_cli_using_command $cmd" -a $subcmd -d "$desc"
+    end
 end
 
