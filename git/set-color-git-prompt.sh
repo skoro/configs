4e29ef86c5eb933e5996038889cf9e7bdb372887
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2022-07-16 13:46:14 +0300

    Customize PS1 and additional git status.

--- git/set-color-git-prompt.sh
+++ git/set-color-git-prompt.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-. git-prompt.sh
+source git-prompt.sh
 
 # Color reference -> ../bash/ansi_colors_echo
-PS1='\e[32m\u@\h \e[34m\W\e[35m$(__git_ps1 " (%s)")\e[39m\$ '
+# In .profile or .bash_profile set the following variables:
+export PS1='\e[32m\u@\h \e[94m\w \e[1m\e[93m$(__git_ps1 "git:%s")\e[22m\e[39m\n\$ '
+export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWUNTRACKEDFILES=1
 
