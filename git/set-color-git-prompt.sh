cb55237e58285656f5109e1db7a9399666310ebb
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2022-07-17 09:10:40 +0300

    bashrc PS1 customization

--- git/set-color-git-prompt.sh
+++ git/set-color-git-prompt.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
-
-source git-prompt.sh
-
 # Color reference -> ../bash/ansi_colors_echo
-# In .profile or .bash_profile set the following variables:
-export PS1='\e[32m\u@\h \e[94m\w \e[1m\e[93m$(__git_ps1 "git:%s")\e[22m\e[39m\n\$ '
-export GIT_PS1_SHOWDIRTYSTATE=1
-export GIT_PS1_SHOWUNTRACKEDFILES=1
+
+# Put below in your $HOME/.bashrc (Ubuntu/Debian).
+GIT_PROMPT=$HOME/.local/git-prompt.sh
+if [ -f $GIT_PROMPT ]; then
+    source $GIT_PROMPT
+	PS1='\e[32m\u@\h \e[94m\w \e[1m\e[93m$(__git_ps1 "git:%s")\e[22m\e[39m\n\$ '
+	GIT_PS1_SHOWDIRTYSTATE=1
+	GIT_PS1_SHOWUNTRACKEDFILES=1
+fi
 
